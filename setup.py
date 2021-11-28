455937eb7c3eb5f85bfb1151667eb8f27403916f
Robert Saunders <robertsaundersiv@gmail.com>
2021-11-28 00:20:01 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Udacity Machine Learning',
+    version='1.0',
+    install_requires=['numpy==1.21.4', 'pandas==1.3.4', 'scikit-learn==1.0.1'],
+    url='',
+    license='',
+    author='Robert Saunders IV',
+    author_email='robertsaundersiv@gmail.com',
+    description='udacity Machine learning project',
+    python_requires='==3.10'
+)
